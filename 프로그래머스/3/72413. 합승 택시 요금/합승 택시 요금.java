e49bce7210e6cc4f20bc33333507e9d16bc19a34
RunSoo <109501116+RunSoo@users.noreply.github.com>
2024-11-10 08:58:27 +0900

    [level 3] Title: 합승 택시 요금, Time: 89.67 ms, Memory: 60.9 MB -BaekjoonHub

--- 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.java
+++ 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.java
@@ -1,40 +1,46 @@
 import java.util.*;
 
 class Solution {
+    public static class Road {
+        int rNum, val;
+        Road(int rNum, int val) {
+            this.rNum = rNum;
+            this.val = val;
+        }
+    }
+    
+    static List<Road>[] fareList;
+    static int[] fromS;
+    
     public int solution(int n, int s, int a, int b, int[][] fares) {
-        int INF = Integer.MAX_VALUE;
+        int answer = 0;
         
-        int answer = INF;
-        
-        int[][] cost = new int[n+1][n+1];
-        for (int r=1; r<n+1; r++) {
-            for (int c=1; c<n+1; c++) {
-                if (r==c) cost[r][c] = 0;
-                else cost[r][c] = INF;
-            }
-        }
-        
-        for (int r=0; r<fares.length; r++) {
-            cost[fares[r][0]][fares[r][1]] = fares[r][2];
-            cost[fares[r][1]][fares[r][0]] = fares[r][2];
-        }
-        
-        for (int k=1; k<n+1; k++) {
-            for (int i=1; i<n+1; i++) {
-                for (int j=1; j<n+1; j++) {
-                    cost[i][j] = Math.min(cost[i][j], (cost[i][k]!=INF && cost[k][j]!=INF) ? cost[i][k]+cost[k][j] : INF);
-                    cost[j][i] = cost[i][j];
-                }
-            }
-        }
-        
+        fareList = new ArrayList[n+1];
         for (int i=1; i<n+1; i++) {
-            answer = Math.min(answer, cost[i][s]+cost[i][a]+cost[i][b]);
+            fareList[i] = new ArrayList<>();
         }
         
-        answer = Math.min(answer, cost[s][a]+cost[s][b]);
+        for (int f=0; f<fares.length; f++) {
+            fareList[fares[f][0]].add(new Road(fares[f][1], fares[f][2]));
+            fareList[fares[f][1]].add(new Road(fares[f][0], fares[f][2]));
+        }
         
+        fromS = new int[n+1]; // 0은 비울게용 s까지 최저비용 구하기
+        for (int i=1; i<n+1; i++) {
+            if (i!=s) fromS[i] = Integer.MAX_VALUE;
+        }
+        
+        for (Road r : fareList[s]) {
+            fromS[r.rNum] = r.val;
+            dfs(r.rNum);
+        }
+        
+        System.out.println(Arrays.toString(fromS));
         
         return answer;
+    }
+    
+    static void dfs(int idx) {
+        
     }
 }
